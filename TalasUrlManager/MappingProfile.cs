544a9e7b349549b9c1c039b2b3e7ff8d6ed7de13
Poy Chang <poy.chang@seec.com.tw>
2017-11-27 17:57:35 +0800

    加入並設定 AutoMapper 套件

---
+++ TalasUrlManager/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace TalasUrlManager
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Add as many of these lines as you need to map your objects
+        }
+    }
+}
--- TalasUrlManager/Startup.cs
+++ TalasUrlManager/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +25,7 @@ namespace TalasUrlManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
