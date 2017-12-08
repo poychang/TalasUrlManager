298f28d4d310b1deb2fa843ff81d176bc2f3ed8b
Poy Chang <poy.chang@seec.com.tw>
2017-12-08 16:38:41 +0800

    建立 footer 區塊

--- TalasUrlManager/ClientApp/src/app/layout/layout.module.ts
+++ TalasUrlManager/ClientApp/src/app/layout/layout.module.ts
@@ -4,6 +4,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 
 import {MyMaterialModule} from '../shared/my-material.module';
 
+import {FooterComponent} from './footer/footer.component';
 import {HeaderComponent} from './header/header.component';
 import {
   ShortenInputSectionComponent
@@ -12,7 +13,9 @@ import {ShortenListSectionComponent} from './shorten-list-section/shorten-list-s
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, MyMaterialModule],
-  exports: [HeaderComponent, ShortenInputSectionComponent, ShortenListSectionComponent],
-  declarations: [HeaderComponent, ShortenInputSectionComponent, ShortenListSectionComponent]
+  exports:
+    [HeaderComponent, ShortenInputSectionComponent, ShortenListSectionComponent, FooterComponent],
+  declarations:
+    [HeaderComponent, ShortenInputSectionComponent, ShortenListSectionComponent, FooterComponent]
 })
 export class LayoutModule {}
