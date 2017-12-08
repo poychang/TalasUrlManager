24dc8304e71ef64b34a1a319ab76fc4b7913ec32
Poy Chang <poy.chang@seec.com.tw>
2017-12-08 15:48:36 +0800

    重構短網址的輸入區塊，加入資料驗證

--- TalasUrlManager/ClientApp/src/app/layout/layout.module.ts
+++ TalasUrlManager/ClientApp/src/app/layout/layout.module.ts
@@ -1,4 +1,6 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {MyMaterialModule} from '../shared/my-material.module';
 
@@ -6,7 +8,7 @@ import {HeaderComponent} from './header/header.component';
 import {ShortenInputSectionComponent} from './shorten-input-section/shorten-input-section.component';
 
 @NgModule({
-  imports: [MyMaterialModule],
+  imports: [CommonModule, ReactiveFormsModule, MyMaterialModule],
   exports: [HeaderComponent, ShortenInputSectionComponent],
   declarations: [HeaderComponent, ShortenInputSectionComponent]
 })
--- TalasUrlManager/ClientApp/src/app/layout/shorten-input-section/shorten-input-section.component.ts
+++ TalasUrlManager/ClientApp/src/app/layout/shorten-input-section/shorten-input-section.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormControl} from '@angular/forms/src/model';
 
 @Component({
   selector: 'app-shorten-input-section',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shorten-input-section.component.scss']
 })
 export class ShortenInputSectionComponent implements OnInit {
+  form: FormGroup;
   shortenDomain = 'shorten.domain/';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
   }
 
+  ngOnInit() {}
+  createForm() {
+    const urlRegex =
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+    this.form = this.formBuilder.group(
+        {originUrl: ['', [Validators.required, Validators.pattern(urlRegex)]]});
+  }
+  getErrorMessage(formControl: FormControl) {
+    switch (Object.keys(formControl.errors)[0]) {
+      case 'required':
+        return '必填欄位';
+      case 'pattern':
+        return '格式錯誤';
+    }
+  }
+  generate() {
+    console.log(`產生 ${this.form.controls['originUrl'].value} 的短網址`);
+  }
 }
