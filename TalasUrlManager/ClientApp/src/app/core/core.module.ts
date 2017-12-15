7fa4227be74c311f3c908f75e55980ac4c5a3080
Poy Chang <poy.chang@seec.com.tw>
2017-12-15 09:47:54 +0800

    新增 ShortenDataService

--- TalasUrlManager/ClientApp/src/app/core/core.module.ts
+++ TalasUrlManager/ClientApp/src/app/core/core.module.ts
@@ -1,8 +1,15 @@
 import {NgModule, Optional, SkipSelf} from '@angular/core';
-import {throwIfAlreadyLoaded} from './module-import-guard';
-import {ShortenDataSource} from './data-source/shorten-data-source';
 
-@NgModule({ imports: [], exports: [], declarations: [], providers: [ShortenDataSource] })
+import {ShortenDataSource} from './data-source/shorten-data-source';
+import {throwIfAlreadyLoaded} from './module-import-guard';
+import {ShortenDataService} from './services/shorten-data.service';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [],
+  providers: [ShortenDataSource, ShortenDataService]
+})
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
