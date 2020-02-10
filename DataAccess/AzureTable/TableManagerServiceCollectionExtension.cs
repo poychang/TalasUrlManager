2d6331f3ad5d6cb73a8f61c33ec95bd783267603
Poy Chang <poypost@gmail.com>
2020-02-10 15:59:23 +0800

    update

---
+++ DataAccess/AzureTable/TableManagerServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Database;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>設定 DbManager 服務的擴展方法</summary>
+    public static class TableManagerServiceCollectionExtension
+    {
+        /// <summary>注入 InMemoryDbManager 服務至指定的 IServiceCollection</summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <param name="setupAction">The middleware configuration options.</param>
+        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection AddTableDbManager(this IServiceCollection services, Action<DbManagerOptions> setupAction = null)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (setupAction != null)
+            {
+                services.Configure(setupAction);
+            }
+
+            services.TryAddSingleton<IDbManager, InMemoryDbManager>();
+
+            return services;
+        }
+    }
+}
--- TalasUrlManager/MappingProfile.cs
+++ TalasUrlManager/MappingProfile.cs
@@ -10,6 +10,7 @@ namespace TalasUrlManager
         {
             // Add as many of these lines as you need to map your objects
             CreateMap<ShortUrlSet, ShortUrlDto>();
+            CreateMap<ShortUrlDto, ShortUrlSet>();
         }
     }
 }
